06b4227af155953111a4c44f0e4e235b1d546f70
eslamal3kda <al3kda1999@gmail.com>
2024-12-26 06:15:22 +0200

    add interval

--- src/pages/UseEffect.jsx
+++ src/pages/UseEffect.jsx
@@ -1,9 +1,20 @@
-import PostsData from "../api/PostsData";
-import ProductsData from "../api/ProductsData";
+import { useEffect, useState } from "react";
+import PostsData from "../components/PostsData";
+import ProductsData from "../components/ProductsData";
 
 export default function UseEffect() {
+    let [counter,setCounter] = useState(0)
+    useEffect(() => {
+        if (counter >= 15) return;
+        const interval = setInterval(() => {
+            setCounter((prev)=> prev + 1)
+        },1000)
+            return ()=> clearInterval(interval)
+    },[counter])
+
     return (
         <>
+            <h2 className="counter">{counter}</h2>
             <PostsData />
             <hr />
             <ProductsData />
